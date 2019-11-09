37710132ce6dc8fb483a4c6c15aadd475df2c107
KangZiNeng <1049335604@qq.com>
2019-11-09 10:17:14 +0800

    张三第二次提交代码

--- 01_helloword/src/helloword.java
+++ 01_helloword/src/helloword.java
@@ -2,4 +2,8 @@ public class helloword {
     public static void main(String[] args) {
         System.out.println("aaaaa");
     }
+
+    public void test1(){
+        System.out.println("bbbbb");
+    }
 }
